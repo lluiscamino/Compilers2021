9744e307839aa80504379fa3cd4cc56b921b476f
Lluís Camino <caminoperez.lluis@gmail.com>
2021-01-03 13:25:47 +0100

    Add tests

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,11 +6,11 @@ import java.util.List;
 import java_cup.runtime.Symbol;
 
 public final class Main {
-    private static final String INPUT_PATH = "C:\\Users\\lluis\\Documents\\NetBeansProjects\\Compiladores2020\\src\\test_program1.txt";
+    private static final String INPUT_PATH = "";
     
     private Main() {}
     
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) {
         try {
             Compiler compiler = new Compiler(INPUT_PATH);
             List<Symbol> tokenList = compiler.getTokenList();
---
+++ test/main/ProgramsTest.java
@@ -0,0 +1,26 @@
+package main;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+public class ProgramsTest {
+
+    private static final String PROGRAMS_PATH = "C:\\Users\\lluis\\Documents\\NetBeansProjects\\Compiladores2020\\test\\programs\\";
+
+    private Compiler getCompiler(String programName) throws FileNotFoundException {
+        return new Compiler(PROGRAMS_PATH + programName);
+    }
+
+    @Test
+    public void testProgram1() throws Exception {
+        Compiler compiler = getCompiler("test_program1.txt");
+        compiler.parse();
+    }
+
+    @Test(expected = Exception.class)
+    public void testProgram2() throws Exception { // Program needs main
+        Compiler compiler = getCompiler("test_program2.txt");
+        compiler.parse();
+    }
+}
