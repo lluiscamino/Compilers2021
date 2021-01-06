f5ccb865424bac9bc93ca3a2cc12e40923a40e45
morj12 <76881131+morj12@users.noreply.github.com>
2021-01-06 07:57:43 +0100

    Update StringLiteral.java

--- src/parser/symbols/expressions/literals/StringLiteral.java
+++ src/parser/symbols/expressions/literals/StringLiteral.java
@@ -20,7 +20,11 @@ public class StringLiteral extends Literal {
 
     @Override
     public void toDot(StringBuilder buffer) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        DotNode dotNode = new DotNode(buffer, "STR_LIT", "", "filled", "#00a2ff");
+        
+        dotNode.addEdge((StringBuilder buffer1) -> {
+            DotNode dotNode1 = new DotNode(buffer1, value, "plaintext", "", "");
+        }, "ident");
     }
     
 }
