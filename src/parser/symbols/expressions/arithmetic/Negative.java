fe7602a98ed39f99005414ef27ad5b88f20875dd
morj12 <76881131+morj12@users.noreply.github.com>
2021-01-07 07:52:18 +0100

    Update Negative.java

--- src/parser/symbols/expressions/arithmetic/Negative.java
+++ src/parser/symbols/expressions/arithmetic/Negative.java
@@ -17,7 +17,11 @@ public final class Negative extends Expression {
 
     @Override
     public void toDot(StringBuilder buffer) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        DotNode dotNode = new DotNode(buffer, "EXPR_NEG", "", "filled", "#00a2ff");
+        
+        dotNode.addEdge((StringBuilder buffer1) -> {
+            DotNode dotNode1 = new DotNode(buffer1, "negExpr", "plaintext", "", "");
+        }, "ident");
     }
     
 }
