9c81ff71d3bfe62c15ec54fc0fa87fa65fcc9a14
morj12 <76881131+morj12@users.noreply.github.com>
2021-01-08 05:29:01 +0100

    Add files via upload

---
+++ src/parser/symbols/expressions/arithmetic/ArithmeticAdd.java
@@ -0,0 +1,30 @@
+package parser.symbols.expressions.arithmetic;
+
+import dot.DotNode;
+import java.io.PrintWriter;
+import parser.symbols.expressions.Expression;
+
+public final class ArithmeticAdd extends Expression {
+    private final Expression leftExpression, rightExpression;
+    
+    public ArithmeticAdd(Expression leftExpression, Expression rightExpression) {
+        this.leftExpression = leftExpression;
+        this.rightExpression = rightExpression;
+    }
+
+    @Override
+    public void validate() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void toDot(StringBuilder buffer) {
+        DotNode dotNode = new DotNode(buffer, "ARITH_ADD", "", "filled", "#00a2ff");
+        
+        dotNode.addEdge((StringBuilder buffer1) -> {
+            DotNode dotNode1 = new DotNode(buffer1, "arithAdd", "plaintext", "", "");
+        }, "ident");
+        dotNode.addEdgeIfNotNull(leftExpression, "leftExpression");
+        dotNode.addEdgeIfNotNull(rightExpression, "rightExpression");
+    }
+}
