9f6f04c9493e81b83381b5fad5b56cb19e70d1f9
morj12 <76881131+morj12@users.noreply.github.com>
2021-01-08 05:29:32 +0100

    Add files via upload

---
+++ src/parser/symbols/expressions/binary/BinaryAnd.java
@@ -0,0 +1,30 @@
+package parser.symbols.expressions.arithmetic;
+
+import dot.DotNode;
+import java.io.PrintWriter;
+import parser.symbols.expressions.Expression;
+
+public final class BinaryAnd extends Expression {
+    private final Expression leftExpression, rightExpression;
+    
+    public BinaryAnd(Expression leftExpression, Expression rightExpression) {
+        this.leftExpression = leftExpression;
+        this.rightExpression = rightExpression;
+    }
+
+    @Override
+    public void validate() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void toDot(StringBuilder buffer) {
+        DotNode dotNode = new DotNode(buffer, "BIN_AND", "", "filled", "#00a2ff");
+        
+        dotNode.addEdge((StringBuilder buffer1) -> {
+            DotNode dotNode1 = new DotNode(buffer1, "binaryAnd", "plaintext", "", "");
+        }, "ident");
+        dotNode.addEdgeIfNotNull(leftExpression, "leftExpression");
+        dotNode.addEdgeIfNotNull(rightExpression, "rightExpression");
+    }
+}
