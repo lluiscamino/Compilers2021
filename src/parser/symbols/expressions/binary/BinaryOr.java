043b6b92c4142e4c3cd0c41fe98903ae783f6559
morj12 <76881131+morj12@users.noreply.github.com>
2021-01-16 15:49:51 +0100

    Update BinaryOr.java

--- src/parser/symbols/expressions/binary/BinaryOr.java
+++ src/parser/symbols/expressions/binary/BinaryOr.java
@@ -12,7 +12,13 @@ public final class BinaryOr extends BinaryOperation {
 
     @Override
     public void validate(SymbolTable symbolTable) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        //mirar si las dos expresiones son booleanas
+        if (!(leftExpression.getType().isBoolean() && rightExpression.getType().isBoolean())) {
+            System.err.println("No se puede hacer una operación lógica con un tipo que no sea booleano");
+        }
+
+        leftExpression.validate(symbolTable);
+        rightExpression.validate(symbolTable);
     }
 
     @Override
