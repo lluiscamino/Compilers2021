dfd416b1d6b75cf797e5d51e3da3fb8704291cf4
morj12 <76881131+morj12@users.noreply.github.com>
2021-01-16 15:47:46 +0100

    Update Priority.java

--- src/parser/symbols/expressions/Priority.java
+++ src/parser/symbols/expressions/Priority.java
@@ -13,7 +13,7 @@ public final class Priority extends Expression {
 
     @Override
     public void validate(SymbolTable symbolTable) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
     }
 
     @Override
