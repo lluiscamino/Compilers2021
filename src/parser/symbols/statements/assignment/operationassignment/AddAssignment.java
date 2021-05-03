0bcd3aab9da7f9ce75992892cdeba5e2980da92a
Lluís Camino <caminoperez.lluis@gmail.com>
2021-05-03 20:31:19 +0200

    Implement AddAssignment::toTac and SubstractAssignment::toTac

--- src/parser/symbols/statements/assignment/operationassignment/AddAssignment.java
+++ src/parser/symbols/statements/assignment/operationassignment/AddAssignment.java
@@ -1,7 +1,11 @@
 package parser.symbols.statements.assignment.operationassignment;
 
 import dot.DotNode;
+import main.Compiler;
 import parser.symbols.expressions.Expression;
+import tac.instructions.arithmetic.AddInstruction;
+import tac.references.TACVariable;
+import tac.tables.VariablesTable;
 
 public final class AddAssignment extends OperationAssignment {
     public AddAssignment(String identifier, Expression expression) {
@@ -15,5 +19,13 @@ public final class AddAssignment extends OperationAssignment {
             new DotNode(identifier, "plaintext", "filled", "");
         }, "ident");
         dotNode.addEdge(expression, "+=");
+    }
+
+    @Override
+    public void toTac() {
+        VariablesTable variablesTable = Compiler.getCompiler().getSemanticAnalyzer().getVariablesTable();
+        TACVariable variable = variablesTable.get(identifier).getTacVariable();
+        expression.toTac();
+        addTACInstruction(new AddInstruction(variable, variable, expression.getTacVariable()));
     }
 }
--- src/parser/symbols/statements/assignment/operationassignment/SubstractAssignment.java
+++ src/parser/symbols/statements/assignment/operationassignment/SubstractAssignment.java
@@ -1,7 +1,11 @@
 package parser.symbols.statements.assignment.operationassignment;
 
 import dot.DotNode;
+import main.Compiler;
 import parser.symbols.expressions.Expression;
+import tac.instructions.arithmetic.SubtractInstruction;
+import tac.references.TACVariable;
+import tac.tables.VariablesTable;
 
 public final class SubstractAssignment extends OperationAssignment {
     public SubstractAssignment(String identifier, Expression expression) {
@@ -15,5 +19,13 @@ public final class SubstractAssignment extends OperationAssignment {
             new DotNode(identifier, "plaintext", "filled", "");
         }, "ident");
         dotNode.addEdge(expression, "-=");
+    }
+
+    @Override
+    public void toTac() {
+        VariablesTable variablesTable = Compiler.getCompiler().getSemanticAnalyzer().getVariablesTable();
+        TACVariable variable = variablesTable.get(identifier).getTacVariable();
+        expression.toTac();
+        addTACInstruction(new SubtractInstruction(variable, variable, expression.getTacVariable()));
     }
 }
