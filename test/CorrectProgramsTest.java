be2b35a37ca6b0e264251c690fc4a345c55598ab
Lluís Camino <caminoperez.lluis@gmail.com>
2021-05-03 20:39:45 +0200

    Use multiline strings

--- test/CorrectProgramsTest.java
+++ test/CorrectProgramsTest.java
@@ -7,193 +7,221 @@ public final class CorrectProgramsTest {
 
     @Test
     public void testArrayAssignment() throws Exception {
-        final String expectedSymbolTable = "1: procedure main (0 args)\n" +
-                "\t2: const INT a\n" +
-                "\t3: const INT_ARR Dimensions:1 b\n" +
-                "\t4: var INT_ARR Dimensions:1 unidimensional\n" +
-                "\t5: var INT_ARR Dimensions:2 bidimensional\n" +
-                "\t6: var INT_ARR Dimensions:3 tridimensional\n" +
-                "\t\t32: var INT i\n" +
-                "\t\t\t33: var INT j\n";
+        final String expectedSymbolTable = """
+                1: procedure main (0 args)
+                \t2: const INT a
+                \t3: const INT_ARR Dimensions:1 b
+                \t4: var INT_ARR Dimensions:1 unidimensional
+                \t5: var INT_ARR Dimensions:2 bidimensional
+                \t6: var INT_ARR Dimensions:3 tridimensional
+                \t\t32: var INT i
+                \t\t\t33: var INT j
+                """;
         testProgram("arrayassignment", expectedSymbolTable);
     }
 
     @Test
     public void testArrayDeclaration() throws Exception {
-        final String expectedSymbolTable = "1: procedure main (0 args)\n" +
-                "\t2: const INT_ARR Dimensions:1 unidimensional\n" +
-                "\t3: const INT_ARR Dimensions:2 bidimensional\n" +
-                "\t4: var INT_ARR Dimensions:3 tridimensional\n";
+        final String expectedSymbolTable = """
+                1: procedure main (0 args)
+                \t2: const INT_ARR Dimensions:1 unidimensional
+                \t3: const INT_ARR Dimensions:2 bidimensional
+                \t4: var INT_ARR Dimensions:3 tridimensional
+                """;
         testProgram("arraydeclaration", expectedSymbolTable);
     }
 
     @Test
     public void testBinarySearch() throws Exception {
-        final String expectedSymbolTable = "1: function binarySearch INT (2 args)\n" +
-                "\t1: var INT_ARR Dimensions:1 nums\n" +
-                "\t1: var INT target\n" +
-                "\t2: var INT left\n" +
-                "\t3: var INT right\n" +
-                "\t4: var INT result\n" +
-                "\t5: var BOOLEAN res\n" +
-                "\t\t7: const INT middle\n" +
-                "21: procedure main (0 args)\n" +
-                "\t22: const INT_ARR Dimensions:1 arr\n" +
-                "\t23: const INT target\n" +
-                "\t24: const INT result\n";
+        final String expectedSymbolTable = """
+                1: function binarySearch INT (2 args)
+                \t1: var INT_ARR Dimensions:1 nums
+                \t1: var INT target
+                \t2: var INT left
+                \t3: var INT right
+                \t4: var INT result
+                \t5: var BOOLEAN res
+                \t\t7: const INT middle
+                21: procedure main (0 args)
+                \t22: const INT_ARR Dimensions:1 arr
+                \t23: const INT target
+                \t24: const INT result
+                """;
         testProgram("binarysearch", expectedSymbolTable);
     }
 
     @Test
     public void testComments() throws Exception {
-        final String expectedSymbolTable = "1: procedure hola (1 args)\n" +
-                "\t1: var INT b\n" +
-                "\t4: var INT a\n" +
-                "8: procedure main (0 args)\n";
+        final String expectedSymbolTable = """
+                1: procedure hola (1 args)
+                \t1: var INT b
+                \t4: var INT a
+                8: procedure main (0 args)
+                """;
         testProgram("comments", expectedSymbolTable);
     }
 
     @Test
     public void testConditional() throws Exception {
-        final String expectedSymbolTable = "1: function f INT (3 args)\n" +
-                "\t1: var BOOLEAN a\n" +
-                "\t1: var BOOLEAN c\n" +
-                "\t1: var BOOLEAN d\n" +
-                "\t2: var INT e\n" +
-                "\t3: const BOOLEAN b\n" +
-                "\t4: var STRING h\n" +
-                "9: function max INT (2 args)\n" +
-                "\t9: var INT a\n" +
-                "\t9: var INT b\n" +
-                "13: function Int2Bool BOOLEAN (1 args)\n" +
-                "\t13: var INT val\n" +
-                "17: procedure main (0 args)\n" +
-                "\t18: var INT f\n";
+        final String expectedSymbolTable = """
+                1: function f INT (3 args)
+                \t1: var BOOLEAN a
+                \t1: var BOOLEAN c
+                \t1: var BOOLEAN d
+                \t2: var INT e
+                \t3: const BOOLEAN b
+                \t4: var STRING h
+                9: function max INT (2 args)
+                \t9: var INT a
+                \t9: var INT b
+                13: function Int2Bool BOOLEAN (1 args)
+                \t13: var INT val
+                17: procedure main (0 args)
+                \t18: var INT f
+                """;
         testProgram("conditional", expectedSymbolTable);
     }
 
     @Test
     public void testFor() throws Exception {
-        final String expectedSymbolTable = "1: procedure loopForever (0 args)\n" +
-                "5: procedure main (0 args)\n" +
-                "\t6: var INT_ARR Dimensions:1 arr\n" +
-                "\t\t7: var INT i\n" +
-                "\t\t10: var INT i\n" +
-                "\t\t13: var INT i\n" +
-                "\t17: var INT i\n" +
-                "\t18: var BOOLEAN repeat\n" +
-                "\t\t25: var INT i\n" +
-                "\t\t25: var INT j\n";
+        final String expectedSymbolTable = """
+                1: procedure loopForever (0 args)
+                5: procedure main (0 args)
+                \t6: var INT_ARR Dimensions:1 arr
+                \t\t7: var INT i
+                \t\t10: var INT i
+                \t\t13: var INT i
+                \t17: var INT i
+                \t18: var BOOLEAN repeat
+                \t\t25: var INT i
+                \t\t25: var INT j
+                """;
         testProgram("for", expectedSymbolTable);
     }
 
     @Test
     public void testForeach() throws Exception {
-        final String expectedSymbolTable = "1: procedure main (0 args)\n" +
-                "\t2: const STRING_ARR Dimensions:1 cities\n" +
-                "\t3: const STRING city\n" +
-                "\t\t4: const STRING city\n" +
-                "\t\t7: var STRING city\n" +
-                "\t\t10: const STRING country\n" +
-                "\t\t13: const INT num\n" +
-                "\t\t16: const BOOLEAN_ARR Dimensions:1 arr\n" +
-                "\t\t\t17: const BOOLEAN val\n";
+        final String expectedSymbolTable = """
+                1: procedure main (0 args)
+                \t2: const STRING_ARR Dimensions:1 cities
+                \t3: const STRING city
+                \t\t4: const STRING city
+                \t\t7: var STRING city
+                \t\t10: const STRING country
+                \t\t13: const INT num
+                \t\t16: const BOOLEAN_ARR Dimensions:1 arr
+                \t\t\t17: const BOOLEAN val
+                """;
         testProgram("foreach", expectedSymbolTable);
     }
 
     @Test
     public void testIf() throws Exception {
-        final String expectedSymbolTable = "1: function loremIpsum BOOLEAN (1 args)\n" +
-                "\t1: var BOOLEAN c\n" +
-                "5: procedure main (0 args)\n" +
-                "\t6: const BOOLEAN condition\n" +
-                "\t7: const BOOLEAN_ARR Dimensions:2 conditions\n";
+        final String expectedSymbolTable = """
+                1: function loremIpsum BOOLEAN (1 args)
+                \t1: var BOOLEAN c
+                5: procedure main (0 args)
+                \t6: const BOOLEAN condition
+                \t7: const BOOLEAN_ARR Dimensions:2 conditions
+                """;
         testProgram("if", expectedSymbolTable);
     }
 
     @Test
     public void testProcedureCall() throws Exception {
-        final String expectedSymbolTable = "1: procedure proc1 (1 args)\n" +
-                "\t1: var STRING text\n" +
-                "6: procedure proc2 (2 args)\n" +
-                "\t6: var INT a\n" +
-                "\t6: var BOOLEAN b\n" +
-                "15: procedure proc3 (1 args)\n" +
-                "\t15: var INT a\n" +
-                "19: procedure proc4 (2 args)\n" +
-                "\t19: var INT num\n" +
-                "\t19: var BOOLEAN b\n" +
-                "25: procedure proc5 (2 args)\n" +
-                "\t25: var INT num1\n" +
-                "\t25: var INT num2\n" +
-                "33: procedure proc6 (1 args)\n" +
-                "\t33: var STRING_ARR Dimensions:1 messages\n" +
-                "38: procedure main (0 args)\n" +
-                "\t39: var STRING text\n" +
-                "\t40: var STRING_ARR Dimensions:1 msgs\n";
+        final String expectedSymbolTable = """
+                1: procedure proc1 (1 args)
+                \t1: var STRING text
+                6: procedure proc2 (2 args)
+                \t6: var INT a
+                \t6: var BOOLEAN b
+                15: procedure proc3 (1 args)
+                \t15: var INT a
+                19: procedure proc4 (2 args)
+                \t19: var INT num
+                \t19: var BOOLEAN b
+                25: procedure proc5 (2 args)
+                \t25: var INT num1
+                \t25: var INT num2
+                33: procedure proc6 (1 args)
+                \t33: var STRING_ARR Dimensions:1 messages
+                38: procedure main (0 args)
+                \t39: var STRING text
+                \t40: var STRING_ARR Dimensions:1 msgs
+                """;
         testProgram("procedurecall", expectedSymbolTable);
     }
 
     @Test
     public void testRelational() throws Exception {
-        final String expectedSymbolTable = "1: procedure main (0 args)\n" +
-                "\t5: const STRING_ARR Dimensions:1 strings\n" +
-                "\t12: var STRING h\n" +
-                "\t13: var INT a\n";
+        final String expectedSymbolTable = """
+                1: procedure main (0 args)
+                \t5: const STRING_ARR Dimensions:1 strings
+                \t12: var STRING h
+                \t13: var INT a
+                """;
         testProgram("relational", expectedSymbolTable);
     }
 
     @Test
     public void testSortStrings() throws Exception {
-        final String expectedSymbolTable = "3: function bubbleSort STRING_ARR Dimensions:1 (1 args)\n" +
-                "\t3: var STRING_ARR Dimensions:1 arr\n" +
-                "\t4: var INT n\n" +
-                "\t5: var BOOLEAN swapped\n" +
-                "\t\t6: var INT i\n" +
-                "\t\t\t8: var INT j\n" +
-                "\t\t\t\t11: var STRING aux\n" +
-                "20: function readStringArray STRING_ARR Dimensions:1 (1 args)\n" +
-                "\t20: var INT len\n" +
-                "\t21: var STRING_ARR Dimensions:1 arr\n" +
-                "\t\t22: var INT i\n" +
-                "\t\t24: var STRING str\n" +
-                "30: procedure main (1 args)\n" +
-                "\t30: var INT arrLength\n" +
-                "\t31: const STRING_ARR Dimensions:1 input\n";
+        final String expectedSymbolTable = """
+                3: function bubbleSort STRING_ARR Dimensions:1 (1 args)
+                \t3: var STRING_ARR Dimensions:1 arr
+                \t4: var INT n
+                \t5: var BOOLEAN swapped
+                \t\t6: var INT i
+                \t\t\t8: var INT j
+                \t\t\t\t11: var STRING aux
+                20: function readStringArray STRING_ARR Dimensions:1 (1 args)
+                \t20: var INT len
+                \t21: var STRING_ARR Dimensions:1 arr
+                \t\t22: var INT i
+                \t\t24: var STRING str
+                30: procedure main (1 args)
+                \t30: var INT arrLength
+                \t31: const STRING_ARR Dimensions:1 input
+                """;
         testProgram("sortstrings", expectedSymbolTable);
     }
 
     @Test
     public void testStringLiteral() throws Exception {
-        final String expectedSymbolTable = "1: procedure empty (1 args)\n" +
-                "\t1: var STRING text\n" +
-                "6: procedure main (0 args)\n" +
-                "\t7: var STRING a\n";
+        final String expectedSymbolTable = """
+                1: procedure empty (1 args)
+                \t1: var STRING text
+                6: procedure main (0 args)
+                \t7: var STRING a
+                """;
         testProgram("stringliteral", expectedSymbolTable);
     }
 
     @Test
     public void twoSum() throws Exception {
-        final String expectedSymbolTable = "1: const INT_ARR Dimensions:1 noResult\n" +
-                "10: function twoSum INT_ARR Dimensions:1 (2 args)\n" +
-                "\t10: var INT_ARR Dimensions:1 nums\n" +
-                "\t10: var INT target\n" +
-                "\t11: var INT_ARR Dimensions:1 result\n" +
-                "\t12: var INT i\n" +
-                "\t13: var BOOLEAN res\n" +
-                "\t\t15: var INT j\n" +
-                "28: procedure main (0 args)\n" +
-                "\t29: const INT_ARR Dimensions:1 result\n";
+        final String expectedSymbolTable = """
+                1: const INT_ARR Dimensions:1 noResult
+                10: function twoSum INT_ARR Dimensions:1 (2 args)
+                \t10: var INT_ARR Dimensions:1 nums
+                \t10: var INT target
+                \t11: var INT_ARR Dimensions:1 result
+                \t12: var INT i
+                \t13: var BOOLEAN res
+                \t\t15: var INT j
+                28: procedure main (0 args)
+                \t29: const INT_ARR Dimensions:1 result
+                """;
         testProgram("twosum", expectedSymbolTable);
     }
 
     @Test
     public void testWhile() throws Exception {
-        final String expectedSymbolTable = "1: procedure doSomething (1 args)\n" +
-                "\t1: var BOOLEAN val\n" +
-                "7: procedure main (0 args)\n" +
-                "\t12: var BOOLEAN condition\n" +
-                "\t13: var INT counter\n";
+        final String expectedSymbolTable = """
+                1: procedure doSomething (1 args)
+                \t1: var BOOLEAN val
+                7: procedure main (0 args)
+                \t12: var BOOLEAN condition
+                \t13: var INT counter
+                """;
         testProgram("while", expectedSymbolTable);
     }
 
