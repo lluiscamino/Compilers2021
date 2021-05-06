814e06b73cc17cc4fe1a3eaee933a53e267d96f5
Robert Mikhailovskiy <76881131+morj12@users.noreply.github.com>
2021-05-06 20:43:03 +0200

    Update ArithmeticMultiply.java

--- src/parser/symbols/expressions/arithmetic/ArithmeticMultiply.java
+++ src/parser/symbols/expressions/arithmetic/ArithmeticMultiply.java
@@ -19,6 +19,10 @@ public final class ArithmeticMultiply extends ArithmeticOperation {
 
     @Override
     public void toTac() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        TACVariableGenerator tacVariableGenerator = Compiler.getCompiler().getSemanticAnalyzer().getTacVariableGenerator();
+        
+        TACVariable t = tacVariableGenerator.generate();  // t = novavar
+        addTACInstruction(new ProductInstruction(t, leftExpression.getTacVariable(), rightExpression.getTacVariable()));  //genera(t = E1.r * E2.r)
+        tacVariable = t;  //E0.r = t;
     }
 }
