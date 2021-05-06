8dd57e80eb15e9566a3e5be262019619274863ad
Robert Mikhailovskiy <76881131+morj12@users.noreply.github.com>
2021-05-06 20:44:45 +0200

    Update BinaryAnd.java

--- src/parser/symbols/expressions/binary/BinaryAnd.java
+++ src/parser/symbols/expressions/binary/BinaryAnd.java
@@ -19,6 +19,10 @@ public final class BinaryAnd extends BinaryOperation {
 
     @Override
     public void toTac() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        TACVariableGenerator tacVariableGenerator = main.Compiler.getCompiler().getSemanticAnalyzer().getTacVariableGenerator();
+        
+        TACVariable t = tacVariableGenerator.generate();  // t = novavar
+        addTACInstruction(new AndInstruction(t, leftExpression.getTacVariable(), rightExpression.getTacVariable()));  //genera(t = E1.r && E2.r)
+        tacVariable = t;  //E0.r = t;
     }
 }
