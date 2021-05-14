dfd608c2e58c5ba70f587b43436ea934dcb78265
Lluís Camino <caminoperez.lluis@gmail.com>
2021-05-14 13:27:45 +0200

    Allow TACReferences as 3rd parameter of AddInstruction

--- src/tac/instructions/arithmetic/AddInstruction.java
+++ src/tac/instructions/arithmetic/AddInstruction.java
@@ -1,11 +1,12 @@
 package tac.instructions.arithmetic;
 
 import assembly.AssemblyCodeGenerator;
+import tac.references.TACReference;
 import tac.references.TACVariable;
 
 public final class AddInstruction extends ArithmeticInstruction {
-    public AddInstruction(TACVariable firstVariable, TACVariable secondVariable, TACVariable thirdVariable) {
-        super(firstVariable, secondVariable, thirdVariable);
+    public AddInstruction(TACVariable firstVariable, TACVariable secondVariable, TACReference thirdReference) {
+        super(firstVariable, secondVariable, thirdReference);
     }
 
     public String toAssemblyCode(AssemblyCodeGenerator codeGenerator) {
