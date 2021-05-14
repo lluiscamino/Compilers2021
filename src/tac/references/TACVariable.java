53649d7eccee14512aaa31a7402159cdb2f17a9b
Lluís Camino <caminoperez.lluis@gmail.com>
2021-05-14 16:45:11 +0200

    Recognize temporal TACVariables

--- src/tac/references/TACVariable.java
+++ src/tac/references/TACVariable.java
@@ -2,15 +2,22 @@ package tac.references;
 
 public final class TACVariable extends TACReference {
     private final String identifier;
+    private final boolean temporal;
 
     public TACVariable(int id) {
         super(id);
         this.identifier = null;
+        this.temporal = true;
     }
 
     public TACVariable(int id, String identifier) {
         super(id);
         this.identifier = identifier;
+        this.temporal = false;
+    }
+
+    public boolean isTemporal() {
+        return temporal;
     }
 
     @Override
