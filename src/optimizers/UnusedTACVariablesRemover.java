b177c1b654f69e09f5da24be8f400c3589aff2ca
Lluís Camino <caminoperez.lluis@gmail.com>
2021-05-15 16:24:28 +0200

    Create UnusedTACVariablesRemover
    Removes unused TACVariables from the VariablesTable

---
+++ src/optimizers/UnusedTACVariablesRemover.java
@@ -0,0 +1,47 @@
+package optimizers;
+
+import tac.instructions.TACInstruction;
+import tac.references.TACReference;
+import tac.references.TACVariable;
+import tac.tables.VariablesTable;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public final class UnusedTACVariablesRemover {
+    private final List<TACInstruction> instructionList;
+    private final VariablesTable variablesTable;
+    private final Set<TACVariable> usedVariables;
+
+    public UnusedTACVariablesRemover(List<TACInstruction> instructionList, VariablesTable variablesTable) {
+        this.instructionList = instructionList;
+        this.variablesTable = variablesTable;
+        this.usedVariables = new HashSet<>();
+    }
+
+    public void removeUnusedVariables() {
+        List<VariablesTable.VariableInfo> removedVariables = new ArrayList<>();
+        for (TACInstruction instruction : instructionList) {
+            addIfVariable(instruction.getFirstReference());
+            addIfVariable(instruction.getSecondReference());
+            addIfVariable(instruction.getThirdReference());
+        }
+        for (VariablesTable.VariableInfo variableInfo : variablesTable.getVariablesList()) {
+            TACVariable variable = variableInfo.getTacVariable();
+            if (!usedVariables.contains(variable)) {
+                removedVariables.add(variableInfo);
+            }
+        }
+        for (VariablesTable.VariableInfo variableInfo : removedVariables) {
+            variablesTable.remove(variableInfo);
+        }
+    }
+
+    private void addIfVariable(TACReference reference) {
+        if (reference instanceof TACVariable) {
+            usedVariables.add((TACVariable) reference);
+        }
+    }
+}
