c06f6c2838651fb5e2adb8999b618f01792fb958
Lluís Camino <caminoperez.lluis@gmail.com>
2021-05-16 20:48:20 +0200

    Update StringLiteral TAC generation

--- src/parser/symbols/expressions/literals/StringLiteral.java
+++ src/parser/symbols/expressions/literals/StringLiteral.java
@@ -4,9 +4,8 @@ import dot.DotNode;
 import java_cup.runtime.ComplexSymbolFactory.Location;
 import parser.symbols.types.Type;
 import tac.generators.TACVariableGenerator;
-import tac.instructions.arithmetic.CopyInstruction;
+import tac.instructions.indexation.IndexAssignmentInstruction;
 import tac.references.TACLiteral;
-import tac.references.TACVariable;
 
 public class StringLiteral extends Literal {
     
@@ -32,7 +31,12 @@ public class StringLiteral extends Literal {
     public void toTac() {
         TACVariableGenerator tacVariableGenerator = main.Compiler.getCompiler().getSemanticAnalyzer().getTacVariableGenerator();
 
-        tacVariable = tacVariableGenerator.generate(Type.getString());  // t = novavar
-        addTACInstruction(new CopyInstruction(tacVariable, new TACLiteral(getValue())));  //genera(t = lit)
+        tacVariable = tacVariableGenerator.generate(Type.getString());
+        String str = getValue();
+        str = str.substring(1, str.length() - 1);
+        for (int i = 0; i < str.length(); i++) {
+            addTACInstruction(new IndexAssignmentInstruction(tacVariable, new TACLiteral(i), new TACLiteral("'" + str.charAt(i) + "'")));
+        }
+        addTACInstruction(new IndexAssignmentInstruction(tacVariable, new TACLiteral(str.length()), new TACLiteral(0)));
     }
 }
