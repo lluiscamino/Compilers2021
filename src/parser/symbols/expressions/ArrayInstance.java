3fe5fe15e4e65d162f5bee2d24577f9c6c7d93fd
Lluís Camino <caminoperez.lluis@gmail.com>
2021-05-31 12:22:00 +0200

    Basic support for ArrayInstance

--- src/parser/symbols/expressions/ArrayInstance.java
+++ src/parser/symbols/expressions/ArrayInstance.java
@@ -7,12 +7,18 @@ import parser.symbols.types.PrimitiveType;
 import parser.symbols.types.Type;
 import tac.generators.TACVariableGenerator;
 import tac.instructions.arithmetic.CopyInstruction;
+import tac.instructions.arithmetic.ProductInstruction;
+import tac.instructions.array.NewArrayInstruction;
+import tac.instructions.indexation.IndexAssignmentInstruction;
 import tac.references.TACLiteral;
+import tac.references.TACVariable;
+
+import java.util.List;
 
 public final class ArrayInstance extends Expression {
     private final PrimitiveType primitiveType;
     private final ArrayIndexes indexes;
-    
+
     public ArrayInstance(PrimitiveType primitiveType, ArrayIndexes indexes) {
         super(Type.getArray(primitiveType, indexes.numIndexes()), Mode.CONST, indexes.xleft);
         this.primitiveType = primitiveType;
@@ -26,7 +32,7 @@ public final class ArrayInstance extends Expression {
     @Override
     public void toDot() {
         DotNode dotNode = new DotNode("ARR_INSTANCE", "box", "filled", "#9077bf");
-        
+
         dotNode.addEdge(getType(), "type");
         dotNode.addEdge(indexes, "indexes");
     }
@@ -35,7 +41,16 @@ public final class ArrayInstance extends Expression {
     public void toTac() {
         TACVariableGenerator variableGenerator = Compiler.getCompiler().getSemanticAnalyzer().getTacVariableGenerator();
 
-        tacVariable = variableGenerator.generate();
-        addTACInstruction(new CopyInstruction(tacVariable, new TACLiteral(primitiveType.defaultValue())));
+        indexes.toTac();
+        tacVariable = variableGenerator.generate(getType());
+        TACVariable arraySizeInBytes = variableGenerator.generate(Type.getInteger());
+        List<Expression> indexesArray = indexes.getIndexes().toArrayList();
+        Expression firstIndex = indexesArray.get(0);
+        PrimitiveType elementsType = indexesArray.size() == 1 ? primitiveType : PrimitiveType.INT;
+
+        addTACInstruction(new CopyInstruction(arraySizeInBytes, firstIndex.getTacVariable()));
+        addTACInstruction(new ProductInstruction(arraySizeInBytes, arraySizeInBytes, new TACLiteral(elementsType.sizeInBytes())));
+        addTACInstruction(new NewArrayInstruction(tacVariable, arraySizeInBytes));
+        addTACInstruction(new IndexAssignmentInstruction(tacVariable, new TACLiteral(0), firstIndex.getTacVariable()));
     }
 }
