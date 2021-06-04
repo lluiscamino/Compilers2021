2b4d1e0eb4c32ac6a54f1a495bd7ecdd5735bd9d
Lluís Camino <caminoperez.lluis@gmail.com>
2021-06-04 19:38:49 +0200

    Avoid code repetition in ArrayOperationAssignment subclasses

--- src/parser/symbols/statements/assignment/operationassignment/ArrayAddAssignment.java
+++ src/parser/symbols/statements/assignment/operationassignment/ArrayAddAssignment.java
@@ -1,26 +1,19 @@
 package parser.symbols.statements.assignment.operationassignment;
 
 import dot.DotNode;
-import main.Compiler;
 import parser.symbols.ArrayIndexes;
 import parser.symbols.expressions.Expression;
-import parser.symbols.types.PrimitiveType;
-import parser.symbols.types.Type;
-import tac.generators.TACVariableGenerator;
 import tac.instructions.arithmetic.AddInstruction;
-import tac.instructions.arithmetic.CopyInstruction;
-import tac.instructions.arithmetic.ProductInstruction;
-import tac.instructions.indexation.IndexAssignmentInstruction;
-import tac.instructions.indexation.IndexedValueInstruction;
-import tac.references.TACLiteral;
 import tac.references.TACVariable;
-import tac.tables.VariablesTable;
-
-import java.util.List;
 
 public final class ArrayAddAssignment extends ArrayOperationAssignment {
     public ArrayAddAssignment(String identifier, ArrayIndexes indexes, Expression expression) {
         super(identifier, indexes, expression);
+    }
+
+    @Override
+    public void operationToTac(TACVariable variable) {
+        addTACInstruction(new AddInstruction(variable, variable, expression.getTacVariable()));
     }
 
     @Override
@@ -30,34 +23,5 @@ public final class ArrayAddAssignment extends ArrayOperationAssignment {
             new DotNode(identifier, "plaintext", "filled", "");
         }, "ident");
         dotNode.addEdge(expression, "+=");
-    }
-
-    @Override
-    public void toTac() {
-        VariablesTable variablesTable = Compiler.getCompiler().getSemanticAnalyzer().getVariablesTable();
-        TACVariableGenerator variableGenerator = Compiler.getCompiler().getSemanticAnalyzer().getTacVariableGenerator();
-
-        VariablesTable.VariableInfo variableInfo = variablesTable.get(identifier);
-        TACVariable variable = variableInfo.getTacVariable();
-        TACVariable newVariable = variableGenerator.generate(variableInfo.getType());
-        TACVariable realIndex = variableGenerator.generate(Type.getInteger());
-        indexes.toTac();
-        List<Expression> indexesList = indexes.getIndexes().toArrayList();
-        TACVariable lastIndex = indexesList.get(indexesList.size() - 1).getTacVariable();
-
-        addTACInstruction(new CopyInstruction(newVariable, variable));
-        for (int i = 0; i < indexesList.size() - 1; i++) {
-            Expression index = indexesList.get(i);
-            addTACInstruction(new AddInstruction(realIndex, index.getTacVariable(), new TACLiteral(1)));
-            addTACInstruction(new ProductInstruction(realIndex, realIndex, new TACLiteral(PrimitiveType.INT.sizeInBytes())));
-            addTACInstruction(new IndexedValueInstruction(newVariable, newVariable, realIndex));
-        }
-        expression.toTac();
-        TACVariable temp = variableGenerator.generate(Type.getInteger());
-        addTACInstruction(new AddInstruction(realIndex, lastIndex, new TACLiteral(1)));
-        addTACInstruction(new ProductInstruction(realIndex, realIndex, new TACLiteral(PrimitiveType.INT.sizeInBytes())));
-        addTACInstruction(new IndexedValueInstruction(temp, newVariable, realIndex));
-        addTACInstruction(new AddInstruction(temp, temp, expression.getTacVariable()));
-        addTACInstruction(new IndexAssignmentInstruction(newVariable, realIndex, temp));
     }
 }
--- src/parser/symbols/statements/assignment/operationassignment/ArrayOperationAssignment.java
+++ src/parser/symbols/statements/assignment/operationassignment/ArrayOperationAssignment.java
@@ -1,12 +1,25 @@
 package parser.symbols.statements.assignment.operationassignment;
 
+import main.Compiler;
 import parser.symbols.ArrayIndexes;
 import parser.symbols.declarations.cva.ArrayDeclaration;
 import parser.symbols.declarations.cva.CVADeclaration;
 import parser.symbols.expressions.Expression;
 import parser.symbols.statements.assignment.ArrayAssignment;
+import parser.symbols.types.PrimitiveType;
 import parser.symbols.types.Type;
 import symboltable.SymbolTable;
+import tac.generators.TACVariableGenerator;
+import tac.instructions.arithmetic.AddInstruction;
+import tac.instructions.arithmetic.CopyInstruction;
+import tac.instructions.arithmetic.ProductInstruction;
+import tac.instructions.indexation.IndexAssignmentInstruction;
+import tac.instructions.indexation.IndexedValueInstruction;
+import tac.references.TACLiteral;
+import tac.references.TACVariable;
+import tac.tables.VariablesTable;
+
+import java.util.List;
 
 public abstract class ArrayOperationAssignment extends ArrayAssignment {
     public ArrayOperationAssignment(String identifier, ArrayIndexes indexes, Expression expression) {
@@ -28,6 +41,37 @@ public abstract class ArrayOperationAssignment extends ArrayAssignment {
             expression.validate();
         }
     }
+
+    @Override
+    public void toTac() {
+        VariablesTable variablesTable = Compiler.getCompiler().getSemanticAnalyzer().getVariablesTable();
+        TACVariableGenerator variableGenerator = Compiler.getCompiler().getSemanticAnalyzer().getTacVariableGenerator();
+
+        VariablesTable.VariableInfo variableInfo = variablesTable.get(identifier);
+        TACVariable variable = variableInfo.getTacVariable();
+        TACVariable newVariable = variableGenerator.generate(variableInfo.getType());
+        TACVariable realIndex = variableGenerator.generate(Type.getInteger());
+        indexes.toTac();
+        List<Expression> indexesList = indexes.getIndexes().toArrayList();
+        TACVariable lastIndex = indexesList.get(indexesList.size() - 1).getTacVariable();
+
+        addTACInstruction(new CopyInstruction(newVariable, variable));
+        for (int i = 0; i < indexesList.size() - 1; i++) {
+            Expression index = indexesList.get(i);
+            addTACInstruction(new AddInstruction(realIndex, index.getTacVariable(), new TACLiteral(1)));
+            addTACInstruction(new ProductInstruction(realIndex, realIndex, new TACLiteral(PrimitiveType.INT.sizeInBytes())));
+            addTACInstruction(new IndexedValueInstruction(newVariable, newVariable, realIndex));
+        }
+        expression.toTac();
+        TACVariable temp = variableGenerator.generate(Type.getInteger());
+        addTACInstruction(new AddInstruction(realIndex, lastIndex, new TACLiteral(1)));
+        addTACInstruction(new ProductInstruction(realIndex, realIndex, new TACLiteral(PrimitiveType.INT.sizeInBytes())));
+        addTACInstruction(new IndexedValueInstruction(temp, newVariable, realIndex));
+        operationToTac(temp);
+        addTACInstruction(new IndexAssignmentInstruction(newVariable, realIndex, temp));
+    }
+
+    public abstract void operationToTac(TACVariable variable);
     
     protected void checkTypeIsInteger(CVADeclaration declaration) throws Exception {
         Type expectedType = getExpectedType((ArrayDeclaration) declaration);
